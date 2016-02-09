5225ef63873bf29d80ca20a812d725e4c17bf324
davenaugler <dv.naugler@gmail.com>
2016-02-08 20:51:11 -0700

    Completed First List

--- app/components/chores/chores-controller.js
+++ app/components/chores/chores-controller.js
@@ -3,4 +3,58 @@
 app.controller('choresController', function($scope){
     $scope .test = "This is a test";
     
+    $scope.Chores = [{
+        task:'Walk dog', 
+        dueDate: Date.now(),
+        responsibleParty: 'Scott',
+        estTime: 15
+        
+    },
+    {
+        task:'Run dog', 
+        dueDate: Date.now(),
+        responsibleParty: 'Dave',
+        estTime: 45
+        
+    },
+    {
+        task:'Do Laundry', 
+        dueDate: Date.now(),
+        responsibleParty: 'Vina',
+        estTime: 30
+        
+    },
+    {
+        task:'Vacuum', 
+        dueDate: Date.now(),
+        responsibleParty: 'Igor',
+        estTime: 25
+        
+    },
+    {
+        task:'Wash Floor', 
+        dueDate: Date.now(),
+        responsibleParty: 'Alex',
+        estTime: 60
+        
+    },
+    {
+        task:'Take out the Garbage', 
+        dueDate: Date.now(),
+        responsibleParty: 'Sarah',
+        estTime: 10
+        
+    },]
+    
+    $scope.addNewChore = function int(){
+        $scope.Chores.push($scope.newTask)
+        $scope.newTask = "";
+        
+    }
+    
+    $scope.deleteChore = function(i){
+        $scope.Chores.splice(i,1)
+        
+    }
+    
 })
